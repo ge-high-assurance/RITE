ba25bdd60e77a2c0255d5967a8ac9af4d5513b85
Christopher Alexander <christopher.alexander@ge.com>
2023-11-01 10:44:36 -0400

    RITE-65: RACK Nature Boiler-Plate

---
+++ tools/rack/rack.plugin/src/com/ge/research/rack/nature/RackBuilder.java
@@ -0,0 +1,17 @@
+package com.ge.research.rack.nature;
+
+import java.util.Map;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class RackBuilder extends IncrementalProjectBuilder {
+
+	@Override
+	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
