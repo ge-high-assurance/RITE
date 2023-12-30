e79b8fd3b3f684dc51af001b41de9b1f2d8a01a4
davidcok <davidcok@github.com>
2023-12-29 20:06:22 -0500

    Adding a Goal View

--- tools/rack/rack.plugin/src/com/ge/research/rack/views/AssuranceGoalView.java
+++ tools/rack/rack.plugin/src/com/ge/research/rack/views/AssuranceGoalView.java
@@ -31,6 +31,7 @@
  */
 package com.ge.research.rack.views;
 
+import com.ge.research.rack.LoadAssuranceCaseHandler;
 import com.ge.research.rack.autoGsn.viewHandlers.AutoGsnUnifiedMainViewHandler;
 import com.ge.research.rack.utils.*;
 import com.ge.research.rack.utils.TreeNode;
@@ -43,16 +44,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.action.*;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.*;
+import org.eclipse.ui.part.*;
 import org.eclipse.ui.part.ViewPart;
 import org.osgi.framework.Bundle;
 
@@ -60,10 +71,15 @@ import java.awt.Frame;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 
 /**
@@ -79,21 +95,21 @@ import javax.swing.JViewport;
  *
  * <p>
  */
-public class AssuranceCaseTree extends ViewPart {
+public class AssuranceGoalView extends ViewPart {
 
     /** The ID of the view as specified by the extension. */
-    public static final String ID = "rackplugin.views.AssuranceCaseTree";
+    public static final String ID = "rackplugin.views.AssuranceGoalView";
 
     @Inject IWorkbench workbench;
     public static HashMap<TreeNode, ArrayList<TreeNode>> graph = null;
     public static HashMap<String, TreeNode> index = null;
-    private TreeViewer viewer;
+//    private TreeViewer viewer;
 //    private DrillDownAdapter drillDownAdapter;
 //    private Action action1;
 //    private Action action2;
 //    private Action doubleClickAction;
     
-    private AutoGsnUnifiedMainViewHandler obj;
+//    private AutoGsnUnifiedMainViewHandler obj;
 
 //    class TreeObject implements IAdaptable {
 //        private String name;
@@ -288,26 +304,6 @@ public class AssuranceCaseTree extends ViewPart {
     @Override
     public void createPartControl(Composite parent) {
 
-        //        viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-        //        drillDownAdapter = new DrillDownAdapter(viewer);
-        //
-        //        viewer.setContentProvider(new ViewContentProvider());
-        //        viewer.setInput(getViewSite());
-        //        viewer.setLabelProvider(new ViewLabelProvider());
-        //
-        //        // Create the help context id for the viewer's control
-        //        if (index == null) {
-        //            return;
-        //        }
-        //
-        //        // workbench.getHelpSystem().setHelp(viewer.getControl(), "rack.plugin.viewer");
-        //        getSite().setSelectionProvider(viewer);
-        //        makeActions();
-        //        hookContextMenu();
-        //        hookDoubleClickAction();
-        //        contributeToActionBars();
-        //    }
-
 //    	obj = new AutoGsnUnifiedMainViewHandler();
 //    	obj.initialize();
     	
@@ -320,27 +316,6 @@ public class AssuranceCaseTree extends ViewPart {
 
         var vp = new JViewport();
         final JFXPanel fxPanel = new JFXPanel();
-
-//        final TabPane tPane = new TabPane();
-//        Tab firstTab = new Tab("First Tab");
-//
-//        GridPane gPane = new GridPane();
-//        gPane.setHgap(10.0);
-//        gPane.setVgap(10.0);
-//
-//        final TextField tField = new TextField();
-//        tField.setPrefWidth(100.0);
-//        tField.setEditable(true);
-//        tField.managedProperty().bind(tField.editableProperty());
-//        tField.setStyle("-fx-border-color: black; -fx-border-width: 2");
-//
-//        Label userName = new Label("User Name");
-//
-//        gPane.add(userName, 0, 0);
-//        gPane.add(tField, 1, 0);
-//
-//        firstTab.setContent(gPane);
-//        tPane.getTabs().add(firstTab);
 
         Platform.runLater(
             new Runnable() {
@@ -355,7 +330,7 @@ public class AssuranceCaseTree extends ViewPart {
             					FileLocator.find(
             							bundle,
             							new Path(
-            									"resources/fxml/autoGsn/AutoGsnUnifiedMainView2.fxml"
+            									"resources/fxml/autoGsn/AutoGsnUnifiedDrillGoalView2.fxml"
             									//"resources/demo.fxml"
             									),
             							null);
@@ -391,96 +366,12 @@ public class AssuranceCaseTree extends ViewPart {
             }
         );
     }
-//
-//    private void hookContextMenu() {
-//        MenuManager menuMgr = new MenuManager("#PopupMenu");
-//        menuMgr.setRemoveAllWhenShown(true);
-//        menuMgr.addMenuListener(
-//                new IMenuListener() {
-//                    public void menuAboutToShow(IMenuManager manager) {
-//                        AssuranceCaseTree.this.fillContextMenu(manager);
-//                    }
-//                });
-//        Menu menu = menuMgr.createContextMenu(viewer.getControl());
-//        viewer.getControl().setMenu(menu);
-//        getSite().registerContextMenu(menuMgr, viewer);
-//    }
-//
-//    private void contributeToActionBars() {
-//        IActionBars bars = getViewSite().getActionBars();
-//        fillLocalPullDown(bars.getMenuManager());
-//        fillLocalToolBar(bars.getToolBarManager());
-//    }
-//
-//    private void fillLocalPullDown(IMenuManager manager) {
-//        manager.add(action1);
-//        manager.add(new Separator());
-//        manager.add(action2);
-//    }
-//
-//    private void fillContextMenu(IMenuManager manager) {
-//        manager.add(action1);
-//        manager.add(action2);
-//        manager.add(new Separator());
-//        drillDownAdapter.addNavigationActions(manager);
-//        // Other plug-ins can contribute there actions here
-//        manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-//    }
-//
-//    private void fillLocalToolBar(IToolBarManager manager) {
-//        manager.add(action1);
-//        manager.add(action2);
-//        manager.add(new Separator());
-//        drillDownAdapter.addNavigationActions(manager);
-//    }
 
-//    private void makeActions() {
-//        action1 =
-//                new Action() {
-//                    public void run() {
-//                        showMessage("Action 1 executed");
-//                    }
-//                };
-//        action1.setText("Action 1");
-//        action1.setToolTipText("Action 1 tooltip");
-//        /*
-//         * action1.setImageDescriptor( PlatformUI.getWorkbench() .getSharedImages()
-//         * .getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//         */
-//
-//        action2 =
-//                new Action() {
-//                    public void run() {
-//                        showMessage("Action 2 executed");
-//                    }
-//                };
-//        action2.setText("Action 2");
-//        action2.setToolTipText("Action 2 tooltip");
-//        // action2.setImageDescriptor(
-//        // workbench.getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//        doubleClickAction =
-//                new Action() {
-//                    public void run() {
-//                        IStructuredSelection selection = viewer.getStructuredSelection();
-//                        Object obj = selection.getFirstElement();
-//                        showMessage("Double-click detected on " + obj.toString());
-//                    }
-//                };
-//    }
 
-//    private void hookDoubleClickAction() {
-//        viewer.addDoubleClickListener(
-//                new IDoubleClickListener() {
-//                    public void doubleClick(DoubleClickEvent event) {
-//                        doubleClickAction.run();
-//                    }
-//                });
+//    private void showMessage(String message) {
+//        MessageDialog.openInformation(
+//                viewer.getControl().getShell(), "Assurance Case GSN", message);
 //    }
-
-    private void showMessage(String message) {
-        MessageDialog.openInformation(
-                viewer.getControl().getShell(), "Assurance Case GSN", message);
-    }
 
     @Override
     public void setFocus() {
@@ -488,27 +379,32 @@ public class AssuranceCaseTree extends ViewPart {
     }
     
     @FXML
-    private void comboFilterAction(ActionEvent event) throws Exception {
-    	ErrorMessageUtil.print("comboFilterAction");
+    private void btnUpAction(ActionEvent event) throws Exception {
+    	ErrorMessageUtil.print("btnUpAction");
     }
 
     @FXML
-    private void btnFetchGoalsAction(ActionEvent event) throws Exception {
-    	ErrorMessageUtil.print("btnFetchGoalsAction");
+    private void btnRootAction(ActionEvent event) throws Exception {
+    	ErrorMessageUtil.print("btnRootAction");
     }
 
     @FXML
-    private void btnGenerateAction(ActionEvent event) throws Exception {
-    	ErrorMessageUtil.print("btnGenerateAction");
+    private void btnHomeAction(ActionEvent event) throws Exception {
+    	ErrorMessageUtil.print("btnHomeAction");
     }
 
     @FXML
-    private void btnTraverseAction(ActionEvent event) throws Exception {
-    	ErrorMessageUtil.print("btnTraverseAction");
+    private void btnTreePopAction(ActionEvent event) throws Exception {
+    	ErrorMessageUtil.print("btnTreePopAction");
     }
 
     @FXML
-    private void listGoalsSelectAction(MouseEvent event) {
-    	ErrorMessageUtil.print("listGoalsSelectAction");
+    private void comboPassFailAction(ActionEvent event) throws Exception {
+    	ErrorMessageUtil.print("comboPassFailAction");
     }
-}
+
+
+    @FXML
+    private void listSubGoalsSelectAction(MouseEvent event) {
+    	ErrorMessageUtil.print("listSubGoalsSelectAction");
+    }}
